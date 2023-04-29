95cd44cbb9ac52439a7aec4e07fd900dff48d83b
Jonathan González <Jonathan Gonzlez@DESKTOP-BJF9U7R>
2023-04-29 11:48:07 -0600

    numeroMayor

--- Ejercicio_9.Designer.cs
+++ Ejercicio_9.Designer.cs
@@ -96,6 +96,7 @@
             this.button1.TabIndex = 9;
             this.button1.Text = "VERIFICAR NOTAS";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
--- Ejercicio_9.cs
+++ Ejercicio_9.cs
@@ -16,5 +16,39 @@ namespace ISNP100522_UNIDAD_2
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double num1, num2;
+
+            if (double.TryParse(txtPrimerNumero.Text, out num1))
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("Ingrese un numero en el Primer contenedor");
+                return;
+            }
+
+            if (double.TryParse(txtSegundoNumero.Text, out num2))
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("Ingrese un numero en el segundo contenedor");
+                return;
+            }
+
+            if (num1 > num2)
+            {
+                MessageBox.Show("El numero Mayor es: " + num1);
+            }
+            else
+            {
+                MessageBox.Show("El numero Mayor es:" + num2);
+            }
+        }
     }
 }
